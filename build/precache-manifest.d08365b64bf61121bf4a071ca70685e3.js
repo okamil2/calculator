a2dc56bea4e2b42052a212f433d880bdf194e66a
okamil2 <okamil2@uwo.ca>
2020-09-21 22:54:43 -0400

    first commit

---
+++ build/precache-manifest.d08365b64bf61121bf4a071ca70685e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8b595c39877b0b6f38d65ea4bd4da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e8fbb45532fbcfc5ea",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f03809d671dd8c4c5389",
+    "url": "/static/js/2.e9ae1c80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9ae1c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e8fbb45532fbcfc5ea",
+    "url": "/static/js/main.9ddf8a8a.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
